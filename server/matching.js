ce1796c07650dad78f37ff3e20fdbd4605323839
Yicheng <yichengs@princeton.edu>
2014-11-15 22:56:24 -0500

    test

---
+++ server/matching.js
@@ -0,0 +1,16 @@
+// var WEDNESDAY = 4;
+// var FRIDAY = 5
+
+// var day_delay = 86400000;
+// var min_delay = 60000;
+
+
+
+// Meteor.setInterval(match, day_delay)
+
+// function match() {
+// 	var curr_date = new Date();
+// 	if (curr_date.getDay() = WEDNESDAY ) {
+// 		console.log('match success');
+// 	}
+// }
